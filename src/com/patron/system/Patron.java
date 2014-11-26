8fcf4cdf4babd2bda2a7ce99208370b2f7030326
James Whiteman <jameswhiteman@outlook.com>
2014-11-25 19:09:53 -0800

    adding libraries and new system files

---
+++ src/com/patron/system/Patron.java
@@ -0,0 +1,35 @@
+package com.patron.system;
+
+import com.urbanairship.AirshipConfigOptions;
+import com.urbanairship.Logger;
+import com.urbanairship.UAirship;
+import com.urbanairship.push.PushManager;
+
+import com.parse.Parse;
+import com.parse.PushService;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.patron.main.FlashScan;
+
+public class Patron extends Application
+{
+	private static Context context;
+	
+	@Override
+	public void onCreate()
+	{
+		super.onCreate();
+		context = this;
+		Parse.initialize(this, "l6vtkQv0R6uPJrXSCn2fiAMRjMFrH6iSA3FQtvkN", "TFzHYXvM4LbuyfqtQsigWaz1j9stCuvcho1zZyge");
+
+		// Also in this method, specify a default Activity to handle push notifications
+  		PushService.setDefaultPushCallback(this, FlashScan.class);
+	}
+	
+	public static Context getContext()
+	{
+		return context;
+	}
+}
